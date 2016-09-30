033d086e7ce93c5199e7447966898dd72ef47f4f
Mirre Abdi <abdi0167@algonquinlive.com>
2016-09-30 01:51:17 -0400

    Commiing after lab 3 was supposed to be demod, and it didnt work, and Eric as a saint and helped, now it works

--- app/src/main/java/com/example/mirre/lab1/ListItemsActivity.java
+++ app/src/main/java/com/example/mirre/lab1/ListItemsActivity.java
@@ -1,13 +1,43 @@
 package com.example.mirre.lab1;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ListItemsActivity extends AppCompatActivity {
-
+    public static final String ACTIVITY_NAME = "StartActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "were in onResume");
+    }
+
+    @Override
+    protected void onStart(){
+
+
+        super.onStart();
+
+        Log.i(ACTIVITY_NAME, "were in onResume");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "were in onStop");
+
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "onDestry");
+
     }
 }
